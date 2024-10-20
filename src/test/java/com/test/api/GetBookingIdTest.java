e10a5a45683f3a8add5a7d44095c88082644602c
govind <bhanusegovind@gmail.com>
2024-10-20 14:28:39 +0530

    First Commit

---
+++ src/test/java/com/test/api/GetBookingIdTest.java
@@ -0,0 +1,20 @@
+package com.test.api;
+
+import org.testng.annotations.Test;
+
+public class GetBookingIdTest {
+
+    @Test
+    public void getBookingIdWithoutFilterTest()
+    {
+
+
+
+
+
+
+
+
+    }
+    }
+}
---
+++ src/test/java/com/test/api/HealthCheckTest.java
@@ -0,0 +1,14 @@
+package com.test.api;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+public class HealthCheckTest {
+        @Test
+    public void healthCheckTest(){
+                    given().
+                    when().
+                    get("https://restful-booker.herokuapp.com/ping").
+                    then().
+                    assertThat().
+                    statusCode(201);
+    }
+}
